3570baf41d3d5d37cdcae59e669a2bc2b2dc6fbf
Ritik Arora <ritik@turings.xyz>
2024-11-03 09:00:52 +0530

    routes change

--- Routes/index.js
+++ Routes/index.js
@@ -4,22 +4,28 @@ import * as transactionController from "../Controller/transactionController.js";
 import * as khataController from "../Controller/khataController.js";
 const router = Express.Router();
 
-router.get("/", userController.home);
-// router.get("/get-posts", postController.getAllPost);
-router.get("/user", userController.getUser);
-router.post("/add-user", userController.postUser);
+router.get("/user/", userController.home);
+// router.get("/user/get-posts", postController.getAllPost);
+router.get("/user/user", userController.getUser);
+router.post("/user/add-user", userController.postUser);
 // router.delete("/delete-post", postController.deletePost);
 
-router.get("/", transactionController.home);
-router.get("/get-transactions", transactionController.getAllTransactions);
-router.get("/transaction", transactionController.getTransaction);
-router.post("/add-transaction", transactionController.postTransaction);
-// router.delete("/delete-post", transactionController.deletePost);
+router.get("/transaction", transactionController.home);
+router.get(
+  "/transaction/get-transactions",
+  transactionController.getAllTransactions
+);
+router.get("/transaction/transaction", transactionController.getTransaction);
+router.post(
+  "/transaction/add-transaction",
+  transactionController.postTransaction
+);
+// router.delete("/transaction/delete-post", transactionController.deletePost);
 
-router.get("/", khataController.home);
-router.get("/get-khata", khataController.getAllKhata);
-router.get("/khata", khataController.getKhata);
-router.post("/add-khata", khataController.postKhata);
-// router.delete("/delete-post", khataController.deletePost);
+router.get("/khata", khataController.home);
+router.get("/khata/get-khata", khataController.getAllKhata);
+router.get("/khata/khata", khataController.getKhata);
+router.post("/khata/add-khata", khataController.postKhata);
+// router.delete("/khata/delete-post", khataController.deletePost);
 
 export default router;
